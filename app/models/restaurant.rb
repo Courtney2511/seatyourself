92360a81b1c228a220f1afcd5ee6d7fdb16976b4
COURTNEY <courtneynoonan@me.com>
2016-08-03 12:37:33 -0400

    adding controllers and associations between models

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,3 @@
 class Restaurant < ApplicationRecord
+  has_many :users, through: :reservations
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,27 @@
 Rails.application.routes.draw do
+  get 'reservations/index'
+
+  get 'reservations/show'
+
+  get 'reservations/new'
+
+  get 'reservations/edit'
+
+  get 'restaurants/index'
+
+  get 'restaurants/show'
+
+  get 'restaurants/new'
+
+  get 'restaurants/edit'
+
+  get 'users/index'
+
+  get 'users/show'
+
+  get 'users/new'
+
+  get 'users/edit'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
