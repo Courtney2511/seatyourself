c4e8782e664f535c93fbdebef748dcc58f424a1d
COURTNEY <courtneynoonan@me.com>
2016-08-04 09:26:53 -0400

    adding restaurant and user controllers and resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,27 +1,8 @@
 Rails.application.routes.draw do
-  get 'reservations/index'
 
-  get 'reservations/show'
+  resources :restaurants
 
-  get 'reservations/new'
-
-  get 'reservations/edit'
-
-  get 'restaurants/index'
-
-  get 'restaurants/show'
-
-  get 'restaurants/new'
-
-  get 'restaurants/edit'
-
-  get 'users/index'
-
-  get 'users/show'
-
-  get 'users/new'
-
-  get 'users/edit'
+  resources :users
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
