50e5b3bf5ee0b2c81365390520d2da05b03abe71
COURTNEY <courtneynoonan@me.com>
2016-08-07 15:30:00 -0400

    correcting associations

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,5 +1,6 @@
 class Restaurant < ApplicationRecord
   has_many :users, through: :reservations
+  has_many :reservations
   validates :name, :address, presence: true
-  validates :capacity, numericality: {only_integer: true}   
+  validates :capacity, numericality: {only_integer: true}
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
   has_many :restaurants, through: :reservations
+  has_many :reservations
   validates :name, :email, :password, :password_confirmation, presence: true
 end
