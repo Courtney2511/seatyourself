4bf2c6a2eceec839f09dbea3e1affec9e3f99325
COURTNEY <courtneynoonan@me.com>
2016-08-07 20:22:09 -0400

    adding restaurant hours to model, and adding restaurant seeds

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -3,4 +3,14 @@ class Restaurant < ApplicationRecord
   has_many :reservations
   validates :name, :address, presence: true
   validates :capacity, numericality: {only_integer: true}
+
+
+  #assume all restaurants open at 11 and close at 8
+  def opening_hour
+    11
+  end
+
+  def closing_hour
+    20
+  end
 end
