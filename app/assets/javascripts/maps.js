b8c1b0353a5033b3d39fc62b860fe1fe571b5129
COURTNEY <courtneynoonan@me.com>
2016-08-15 19:53:22 -0400

    stylng map in view, creating more seed data

--- app/assets/javascripts/maps.js
+++ app/assets/javascripts/maps.js
@@ -4,7 +4,32 @@ function initMap(){
   var latLong = { lat: mapPlaceholder.data('latitude'), lng: mapPlaceholder.data('longitude') }
   var map = new google.maps.Map(mapPlaceholder[0], {
     center: latLong,
-    zoom: 16
+    zoom: 16,
+    zoomControl: false,
+    scaleControl: false,
+    scrollwheel: false,
+    disableDoubleClickZoom: true,
+    styles: [
+  {
+    featureType: 'all',
+    stylers: [
+      { saturation: -80 }
+    ]
+  },{
+    featureType: 'road.arterial',
+    elementType: 'geometry',
+    stylers: [
+      { hue: '#00ffee' },
+      { saturation: 50 }
+    ]
+  },{
+    featureType: 'poi.business',
+    elementType: 'labels',
+    stylers: [
+      { visibility: 'off' }
+    ]
+  }
+]
   });
 
   var marker = new google.maps.Marker({
